6bab2c13ef83fdbc52532abae53c2ecf4851447c
Vince Oblak <voblak2@gmail.com>
2020-09-01 14:17:31 -0500

    Created thisApp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace thisApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
